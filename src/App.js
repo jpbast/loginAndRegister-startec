531e785f97cd34183394af69fec4346eaaaac242
Joao <jp_bast@hotmail.com>
2021-04-15 13:17:50 -0300

    configuration files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles/global.css'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-     
+      <Login />
     </div>
   );
 }
